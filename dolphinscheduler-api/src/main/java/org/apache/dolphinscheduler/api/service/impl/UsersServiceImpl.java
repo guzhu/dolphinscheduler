425f1315f56c28b686bfb8b7496c6f72a83085a5
guzhu <wqs510@qq.com>
2022-06-09 11:14:08 +0800

    Update UsersServiceImpl.java
    修改ldap用户默认状态为启用

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/UsersServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/UsersServiceImpl.java
@@ -241,6 +241,7 @@ public class UsersServiceImpl extends BaseServiceImpl implements UsersService {
         user.setCreateTime(now);
         user.setUpdateTime(now);
         user.setQueue("");
+        user.setState(1);
 
         // save user
         userMapper.insert(user);
